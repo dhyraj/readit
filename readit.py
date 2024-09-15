13c2b5748f2eee744d5dfe36f90a869818500a1e
dhyraj <dhiraj.yadav2873@gmail.com>
2024-09-15 21:36:06 +0530

    refactor

--- readit.py
+++ readit.py
@@ -4,6 +4,7 @@ from enum import Enum
 from validations import ValidationService
 from validations import ValidationUtils
 from constants import Constants
+import time
 
 class RedditTimeFilterType(Enum):
     ALL = 'ALL'
@@ -49,6 +50,7 @@ class RedditSortType(Enum):
 class Readit:
     def __init__(self, subreddit:str = None, url:str = None) -> None:
         self._path = None
+        self.pathForMore = "/svc/shreddit/community-more-posts"
         self._url = None
         self.subreddit = None
         self.sort = RedditSortType.NEW
@@ -76,9 +78,9 @@ class Readit:
         self._path = "".join(url.split("reddit.com")[1:])
                 
     
-    def getRedditUrlForPostsAfter(self, after: str = None, feedlength: int= 31):
+    def getRedditUrlForPostsAfter(self, after: str = None, feedlength: int= 100):
         if ValidationUtils.isNotEmptyString(after): 
-            self._url = f'{Constants.domain}/{self.subreddit}/{self.sort.value}/?after={after}%3D%3D&name={self.subreddit[2:]}&feedLength={feedlength}'
+            self._url = f'{Constants.domain}/{self.pathForMore}/{self.sort.value}/?after={after}%3D%3D&name={self.subreddit[2:]}&feedLength={feedlength}'
         else:
             self._url = f'{Constants.domain}/{self.subreddit}/{self.sort.value}'
         return self._url
@@ -94,41 +96,46 @@ class Readit:
         redditUrl = self.getRedditUrlForPostsAfter()
         return self.getSubmissions(redditUrl=redditUrl, limit=limit)
     
-    def getSubmissions(self, redditUrl: str = None, limit: int = 1000):
-        print("url: ", redditUrl)
+    def getSubmissions(self, redditUrl: str = None,size: int = 3, limit: int = 1000):
+        timeOfRequest = time.time_ns()
+        print(f"url ({timeOfRequest}): {redditUrl}")
         response = requests.get(redditUrl, verify= True)
+
+        with open(f"lastResponsePages{timeOfRequest}.html", "w+") as page:
+            page.write(response.text)
+
         if response.status_code == 200:
             redditSoup = BeautifulSoup(response.text, "html.parser")
             posts = redditSoup.find_all("shreddit-post")
             if posts is not None:
                 nextToken, jsonPosts = self.processPosts(posts)
-
+                print(f'loaded: {len(jsonPosts)}')
                 loadMoreUrl = None
                 if ValidationUtils.isNotEmptyString(nextToken):
-                    loadMoreUrl = self.getRedditUrlForPostsAfter(nextToken)
+                    loadMoreUrl = self.getRedditUrlForPostsAfter(nextToken, feedlength=size+25)
                 else:
                     nextPageMetaData= redditSoup.find('faceplate-partial', {'slot': 'load-after'})
                     if nextPageMetaData is not None and nextPageMetaData.attrs is not None and ValidationUtils.isNotEmptyString(nextPageMetaData.attrs['src']):
                         loadMoreUrl = f'{Constants.domain}{nextPageMetaData.attrs["src"]}'
 
                 if ValidationUtils.isNotEmptyString(loadMoreUrl):
-                    nextJsonPosts = self.getSubmissions(loadMoreUrl, limit - len(jsonPosts))
+                    nextJsonPosts = self.getSubmissions(redditUrl=loadMoreUrl, limit=limit - len(jsonPosts), size=size+25)
                     if nextJsonPosts is not None: 
-                        jsonPosts.extend(nextJsonPosts)
+                        jsonPosts.union(nextJsonPosts)
 
                 return jsonPosts
         else:
             return None
     
     def processPosts(self, postsContainer):
-        posts = list()
+        posts = set()
         nextToken = None
         for post in postsContainer:
             if post != None and post.name != None:
                 # convert to json
-                posts.append(post)
-                if post.atts is not None and 'more-posts-cursor' in post.atts:
-                    print("cursor set up")
+                posts.add(post)
+                if post.attrs is not None and 'more-posts-cursor' in post.attrs:
+                    # continue
                     nextToken = post.attrs['more-posts-cursor']
         return nextToken, posts
 
